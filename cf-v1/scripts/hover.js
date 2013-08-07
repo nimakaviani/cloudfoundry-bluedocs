e76d3c1c2083f52a5555518d24008b16684fb507
Nima K <nima.kaviani@gmail.com>
2013-08-07 11:03:00 -0400

    first commit of the docs

---
+++ cf-v1/scripts/hover.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $('div[id^=pop-up]').hide();
+});
+
+$(function() {
+    var moveLeft = 20;
+    var moveDown = 10;
+
+    $('a[id^=trigger]').hover(function(e) {
+      var matches = this.id.match(/\d+/);
+      var numb    = parseInt(matches[0]);
+      var divId   = "div#pop-up"+numb;
+      $(divId).show();
+    }, function() {
+      var matches = this.id.match(/\d+/);
+      var numb    = parseInt(matches[0]);
+      var divId   = "div#pop-up"+numb;
+      if ($(divId).data('is_clicked') === 'false' || $(divId).data('is_clicked') === undefined){        
+        $(divId).hide();
+      }
+    });
+
+    $('a#trigger').mousemove(function(e) {
+      $('div[id^=pop-up]').css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
+    });
+
+    $('a[id^=trigger]').click(function(e) {
+      var matches = this.id.match(/\d+/);
+      var numb    = parseInt(matches[0]);
+      var divId   = "div#pop-up"+numb;
+      e.preventDefault();
+      if (!$(divId).css('visibility') === 'hidden') {
+        $(divId).hide();
+      } else {
+        if ($(divId).data('is_clicked') === 'false' || $(divId).data('is_clicked') === undefined){
+          this.innerHTML = "-";
+          $(divId).data('is_clicked', 'true');
+          $(divId).show();
+        }else{
+          this.innerHTML = "+";
+          $(divId).data('is_clicked', 'false');
+          $(divId).hide();          
+        }
+      }
+    });
+});
